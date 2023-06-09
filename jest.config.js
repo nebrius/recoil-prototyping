668d898add779386a13c286bedc557fe5a6cbe7e
Bryan Hughes <bryan@nebri.us>
2023-06-09 08:41:21 -0700

    Misc cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
@@ -12,16 +9,10 @@ const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleNameMapper: {
         // Not sure why we have to duplicate this info, but it's necessary for now
-        '^components/(.*)$': '<rootDir>/src/components/$1',
-        '^pages/(.*)$': '<rootDir>/src/pages/$1',
-        '^server/(.*)$': '<rootDir>/src/server/$1',
-        '^state/(.*)$': '<rootDir>/src/state/$1',
-        '^test/(.*)$': '<rootDir>/src/test/$1',
-        '^types/(.*)$': '<rootDir>/src/types/$1',
-        '^utils$': '<rootDir>/src/utils.ts',
+        '^packages/(.*)$': '<rootDir>/packages/$1',
     },
     testEnvironment: 'jest-environment-jsdom',
-    coveragePathIgnorePatterns: ['/node_modules/', '**/util.tsx'],
+    coveragePathIgnorePatterns: ['/node_modules/'],
 }
 
 module.exports = createJestConfig(customJestConfig)
--- packages/utils/api.ts
+++ packages/utils/api.ts
@@ -1,6 +1,5 @@
 export async function get<T = never>(endpoint: string) {
   const res = await fetch(`http://localhost:3000/api${endpoint}`);
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return (await res.json()) as T;
 }
 
