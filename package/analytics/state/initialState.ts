3b9b2f8c3029cc3a03fc52c26348e954538aceb1
Bryan Hughes <bryan@nebri.us>
2023-06-09 07:48:25 -0700

    Removed outdated comments

--- package/analytics/state/initialState.ts
+++ package/analytics/state/initialState.ts
@@ -2,18 +2,6 @@ import { atom } from 'recoil';
 
 import { InitialState } from '../types/initialState';
 
-// We take the data returned from the Next.js root server component and stick it
-// into the initialStateAtom atom. We use this atom to initialize other atoms,
-// although indirectly. Since each page can have different data in it, we forbid
-// access to this atom directly, and instead require them to go through page
-// specific selectors. These selectors do two things: 1) they make sure they're
-// only referenced on the page they're intended for and 2) they cast the data so
-// it has the correct TypeScript signature for that page.
 export const initialStateAtom = atom<InitialState>({
   key: 'analytics:initialStateAtom',
-  // No default is set here intentionally. Not setting the default causes this
-  // atom to be set in a "loading" state, enabling React's Suspense that
-  // delays rendering of any component dependent on it. We then set this
-  // initial state in the Recoil root in AnalyticsRoot (which in practice makes
-  // this available before any renders)
 });
