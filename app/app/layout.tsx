f6fd10443cc1d769704b16f0cf8f561edfafff2d
Bryan Hughes <bryan@nebri.us>
2023-06-23 23:50:37 -0400

    Switched app to use abstracted Nav

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,15 +1,5 @@
-import Link from 'next/link';
-
-import styles from './dashboard.module.css';
+import { Nav } from '@rp/common';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className={styles.container}>
-      <nav className={styles.nav}>
-        <Link href="/analytics">Analytics</Link>
-        <Link href="/settings">Settings</Link>
-      </nav>
-      <div className={styles.content}>{children}</div>
-    </div>
-  );
+  return <Nav>{children}</Nav>;
 }
