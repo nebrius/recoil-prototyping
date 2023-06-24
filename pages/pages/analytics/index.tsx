d2a1f94693254e7e7f34fddbe80e76585c6d4f51
Bryan Hughes <bryan@nebri.us>
2023-06-23 23:48:42 -0400

    Ported analytics to pages

--- pages/pages/analytics/index.tsx
+++ pages/pages/analytics/index.tsx
@@ -1,3 +1,45 @@
-export default function Analytics() {
-  return <>Analytics</>;
+import type { AnalyticsBootstrapData } from '@rp/analyticsLayout';
+import { DetailsLayoutRoot } from '@rp/analyticsLayout';
+import type { CommonBootstrapData } from '@rp/common';
+import { AppRoot, Nav } from '@rp/common';
+import { get } from '@rp/utils';
+
+import styles from './styles.module.css';
+
+interface Props {
+  commonBootstrapData: CommonBootstrapData;
+  analyticsBootstrapData: AnalyticsBootstrapData;
+}
+
+export async function getServerSideProps() {
+  const [commonBootstrapData, analyticsBootstrapData] = await Promise.all([
+    get<CommonBootstrapData>('/base'),
+    get<AnalyticsBootstrapData>('/analytics'),
+  ]);
+
+  const props: Props = {
+    commonBootstrapData,
+    analyticsBootstrapData,
+  };
+
+  return { props };
+}
+
+export default function Analytics({
+  commonBootstrapData,
+  analyticsBootstrapData,
+}: Props) {
+  return (
+    <div className={styles.app}>
+      <AppRoot bootstrapData={commonBootstrapData}>
+        <Nav>
+          <h3 className={styles.settingsHeader}>Analytics page</h3>
+          <div>
+            <h4>Details</h4>
+            <DetailsLayoutRoot bootstrapData={analyticsBootstrapData} />
+          </div>
+        </Nav>
+      </AppRoot>
+    </div>
+  );
 }
